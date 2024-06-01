a738adee7234946857717474e5ac463345294ebe
Xoris <xmingwp@gmail.com>
2024-06-01 20:14:17 +0800

    refactor: move the bit operation functions to utils

--- src/common/utils.h
+++ src/common/utils.h
@@ -12,4 +12,14 @@ static inline bool inOr(R cond, U... args) {
   return ((cond == args) || ...);
 }
 
+template<typename T>
+inline static T clearBitN(T val, int n) {
+  return val & ~(1 << (n));
+}
+
+template<typename T>
+inline static T getBitN(T val, int n) {
+  return val >> n & 1;
+}
+
 #define GB_ASSERT(COND) assert((COND))
