d45dc97e4a0b609928f85563a94fbc5f862aaef6
Xoris <xmingwp@gmail.com>
2024-06-03 23:47:38 +0800

    feat: add `setBitN`

--- src/common/utils.h
+++ src/common/utils.h
@@ -14,12 +14,17 @@ static inline bool inOr(R cond, U... args) {
 
 template<typename T>
 inline static T clearBitN(T val, int n) {
-  return val & ~(1 << (n));
+  return val & ~(1 << n);
 }
 
 template<typename T>
 inline static T getBitN(T val, int n) {
   return val >> n & 1;
+}
+
+template<typename T>
+inline static T setBitN(T val, int n) {
+  return clearBitN(val, n) | (1 << n);
 }
 
 #define GB_ASSERT(COND) assert((COND))
