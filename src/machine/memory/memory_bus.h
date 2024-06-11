c21bf2364b0d575ebde4d09726d0fe965bb6788f
Xoris <xmingwp@gmail.com>
2024-06-11 23:00:22 +0800

    feat: add some memories to `MemoryBus`

--- src/machine/memory/memory_bus.h
+++ src/machine/memory/memory_bus.h
@@ -1,5 +1,8 @@
 #include "common/logger.h"
+#include "machine/interrupt.h"
 #include "machine/memory/memory_accessor.h"
+#include "machine/ppu/ppu_register.h"
+#include "machine/timer.h"
 
 namespace gb {
 
@@ -23,13 +26,13 @@ private:
         break;
       case 0x8000 ... 0x9FFF:
         // 8 KiB Video RAM (VRAM) In CGB mode, switchable bank 0/1
-        break;
+        return &vram_;
       case 0xA000 ... 0xBFFF:
         // 8 KiB External RAM From cartridge, switchable bank if any
         break;
       case 0xC000 ... 0xCFFF:
         // 4 KiB Work RAM (WRAM)
-        break;
+        return &wram_;
       case 0xD000 ... 0xDFFF:
         // 4 KiB Work RAM (WRAM) In CGB mode, switchable bank 1–7
         break;
@@ -38,16 +41,16 @@ private:
         break;
       case 0xFE00 ... 0xFE9F:
         // Object attribute memory (OAM)
-        break;
+        return &oam_;
       case 0xFEA0 ... 0xFEFF:
         // Not Usable Nintendo says use of this area is prohibited.
         break;
       case 0xFF80 ... 0xFFFE:
         // High RAM (HRAM)
-        break;
+        return &hram_;
       case 0xFFFF ... 0xFFFF:
         // Interrupt Enable register (IE)
-        break;
+        return &ie_;
       // io registers
       case 0xFF00:
         // Joypad input
@@ -57,7 +60,9 @@ private:
         break;
       case 0xFF04 ... 0xFF07:
         //  Timer and divider
-        break;
+        return timer_;
+      case 0xFF0F:
+        return &if_;
       case 0xFF10 ... 0xFF26:
         //  Audio
         break;
@@ -66,7 +71,7 @@ private:
         break;
       case 0xFF40 ... 0xFF4B:
         //  LCD Control, Status, Position, Scrolling, and Palettes
-        break;
+        return ppu_registers_;
       case 0xFF4F:
         // VRAM Bank Select
         break;
@@ -89,6 +94,14 @@ private:
   }
 
 private:
+  mutable Memory<0xc000, 0xdfff> wram_{};
+  mutable Memory<0x8000, 0x9fff> vram_{};
+  mutable Memory<0xfe00, 0xfe9f> oam_{};
+  mutable Timer* timer_{};
+  mutable InterruptFlag if_;
+  mutable PPURegister* ppu_registers_;
+  mutable Memory<0xff80, 0xfffe> hram_{};
+  mutable InterruptEnable ie_;
 };
 
 } // namespace gb
