e9de4a8b8b0be299bdcfb4238d888a9f7d045033
Xoris <xmingwp@gmail.com>
2024-07-10 22:20:14 +0800

    feat: add class `ObjectAttribute`

---
+++ src/machine/object_attribute.h
@@ -0,0 +1,23 @@
+#include "common/type.h"
+
+namespace gb {
+// OAM: 0xfe00-0xfe9f
+struct ObjectAttribute {
+  u8 y;
+  u8 x;
+  u8 tile_index;
+  u8 attrs;
+
+  u8 priority() const { return attrs >> 7; }
+
+  u8 yFlip() const { return attrs & 0x40; }
+
+  u8 xFlip() const { return attrs & 0x20; }
+
+  u8 dmgPalette() const { return attrs & 0x10; }
+
+  u8 bank() const { return attrs & 0x8; }
+
+  u8 cgbPalette() const { return attrs & 0x7; }
+};
+} // namespace gb
